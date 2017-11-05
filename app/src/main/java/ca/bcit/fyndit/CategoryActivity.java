071a4c0b6d44b7efd126043acd3626be7d287190
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-11-05 01:01:50 -0700

    added Activity Category

---
+++ app/src/main/java/ca/bcit/fyndit/CategoryActivity.java
@@ -0,0 +1,17 @@
+package ca.bcit.fyndit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
+
--- app/src/main/java/ca/bcit/fyndit/MainActivity.java
+++ app/src/main/java/ca/bcit/fyndit/MainActivity.java
@@ -1,7 +1,11 @@
 package ca.bcit.fyndit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickBtn(View view)
+    {
+        String buttonText = ((Button)view).getText().toString();
+        Intent intent = new Intent(this, CategoryActivity.class);
+        intent.putExtra("category", buttonText);
+        Toast.makeText(this, "Clicked on " + buttonText + " button", Toast.LENGTH_LONG).show();
+        startActivity(intent);
     }
 }
