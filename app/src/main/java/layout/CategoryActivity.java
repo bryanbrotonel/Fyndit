eeba6b89f3ecf20cda33d38d128366f0a28ca667
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2017-11-23 12:45:04 -0800

    Loaded locations into LocationDetail set.

--- app/src/main/java/layout/CategoryActivity.java
+++ app/src/main/java/layout/CategoryActivity.java
@@ -5,10 +5,17 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
 import ca.bcit.fyndit.R;
 
 public class CategoryActivity extends AppCompatActivity {
 
+    private ArrayList<LocationDetail> locations = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,17 @@ public class CategoryActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String category = intent.getStringExtra("category");
+        String rawJson = intent.getStringExtra("data");
+
+        try {
+            JSONArray jsonData = new JSONArray(rawJson);
+
+            for (int i = 0; i < jsonData.length(); i++)
+                locations.add(LocationDetail.fromJson(jsonData.getJSONObject(i)));
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar);
